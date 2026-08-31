a7a33aa87b6e8e10edfcf4f455df91084f286146
MithunR <mithunr@nvidia.com>
2026-08-31 14:17:17 -0700

    Review:  Changed default arg in regex_rewrite_utils.
    Signed-off-by: MithunR <mithunr@nvidia.com>

--- src/main/cpp/src/regex_rewrite_utils.hpp
+++ src/main/cpp/src/regex_rewrite_utils.hpp
@@ -42,6 +42,6 @@ std::unique_ptr<cudf::column> literal_range_pattern(
   int const len,
   int const start,
   int const end,
-  cuda::stream_ref stream           = rmm::cuda_stream_default,
+  cuda::stream_ref stream           = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource_ref());
 }  // namespace spark_rapids_jni
